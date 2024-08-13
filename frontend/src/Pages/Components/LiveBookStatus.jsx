23ed67a638cdeedc635f7721c2cd0d329838ee4e
simontamene.dev@gmail.com <simontamene.dev@gmail.com>
2024-08-13 20:28:33 +0300

    BRM

--- frontend/src/Pages/Components/LiveBookStatus.jsx
+++ frontend/src/Pages/Components/LiveBookStatus.jsx
@@ -142,7 +142,7 @@ const LiveBookStatus = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await fetch('https://book-rental-system-qctq.vercel.app/admin/api/getAllbooks');
+        const response = await fetch('https://book-rental-system-qctq.vercel.app/admin/api/getAllbooks',{credentials:"include"});
         const result = await response.json();
         setData(result);
       } catch (error) {
